f82b0235bfa81a747e4f173c0c74488b39deb566
Carlos Mertens <mertenscarlos@yahoo.com>
2021-05-06 15:00:26 +0200

    Add types file

---
+++ types.ts
@@ -0,0 +1,11 @@
+const firstName = 'Carlos';
+const secretNumber = 20;
+const married = true;
+const wifeAge = undefined;
+const today = new Date();
+today.getMonth();
+const person = {
+  age: 20,
+};
+class Color {}
+const red = new Color();
